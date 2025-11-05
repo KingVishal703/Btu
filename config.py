361f46addb04ae01b8bd527eedc929711bb23100
KingVishal703 <vishal21wankhede@gmail.com>
2025-11-05 17:22:50 +0530

    Update bot token and database configuration

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 # Your bot token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7917897990:AAFj1hn2JWJ5pvKNUrbL9sDaKmqug0dlcO8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6335646151:AAF22GWMVxarTI7lTuSRTBwsRA1IvwHrQXw")
 
 # MongoDB (you can use SRV URL here)
 MONGO_URL = os.environ.get(
     "MONGO_URL",
-    "mongodb+srv://teddugovardhan544_db_user:WVjIA96jQ31net0j@cluster0.kwkkleo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://mrsam:mrsam1212@cluster0.br8a94z.mongodb.net/?retryWrites=true&w=majority"
 )
 
 # Your old database and collection
-DB_NAME = os.environ.get("DB_NAME", "filestore")
-COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "users")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "Telegram_files")
 
 # Admin Telegram ID (only admin can broadcast)
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "5654093580"))
